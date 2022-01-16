7aa8bc14cf2cb3e422ac720756911f2afc515343
tomporat247 <tomporat247@gmail.com>
2022-01-16 16:48:20 +0200

    x

--- misc/slack-notification/slack.sh
+++ misc/slack-notification/slack.sh
@@ -16,4 +16,5 @@ fi
 
 ICON_URL="https://uploads-ssl.webflow.com/5ceab5395d0f478e169de7c0/5f21eb54b57c7a8559c68ff7_Env0-Color%20Icon%403x.png"
 PAYLOAD="{\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"webhookbot\", \"text\": \"${MESSAGE}\", \"icon_url\": \"${ICON_URL}\"}"
+
 curl -X POST --data-urlencode "payload=${PAYLOAD}" ${SLACK_URL}
