b38efd66db0fdd915c2ce71684ceee75f5314588
elbaz <eranelbaz97@gmail.com>
2022-05-26 15:59:27 +0300

    Update main.tf

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,2 +1,18 @@
+provider "random" {}
+
+
+resource "random_integer" "time" {
+  min = 1
+  max = 10
+}
+
+
 resource "null_resource" "null" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
+
+resource "time_sleep" "wait" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "${random_integer.time.result}s"
 }
