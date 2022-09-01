010c942e11fcd502fcdc62b0dfc8a8980cfe2b04
tomporat247 <tomporat247@gmail.com>
2022-09-01 12:23:25 +0300

    update config

--- misc/remote-backend-env0/backend.tf
+++ misc/remote-backend-env0/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "backend-pr9181.api.dev.env0.com"
+    hostname = "backend-pr9190.api.dev.env0.com"
     organization = "66d0f339-1ac0-409f-a78b-984272a4a661"
 
     workspaces {
