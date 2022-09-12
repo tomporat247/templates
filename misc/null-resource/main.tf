5e8613357b1ba8bf8d48bc8e215ef6bb92479e6c
tomporat247 <tomporat247@gmail.com>
2022-09-12 18:13:53 +0300

    fix tflint

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,3 +1,19 @@
 resource "null_resource" "null" {
   count = 2
 }
+
+terraform {
+  required_version = "1.2.2"
+
+  required_providers {
+    null_resource = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+  }
+}
+
+
+provider "null_resource" {
+  version = "3.1.1"
+}
