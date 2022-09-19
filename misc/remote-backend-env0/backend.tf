ee792439dc280ac9b8d78d8604a1260cd3a9176d
tomporat247 <tomporat247@gmail.com>
2022-09-19 16:16:57 +0300

    x

--- misc/remote-backend-env0/backend.tf
+++ misc/remote-backend-env0/backend.tf
@@ -1,10 +1,20 @@
+#terraform {
+#  backend "remote" {
+#    hostname = "backend-pr9224.api.dev.env0.com"
+#    organization = "a24d5d61-8c2b-4a58-8917-9a663cf1c4c0"
+#
+#    workspaces {
+#      name = "tom-qa"
+#    }
+#  }
+#}
+
 terraform {
   backend "remote" {
-    hostname = "backend-pr9224.api.dev.env0.com"
-    organization = "a24d5d61-8c2b-4a58-8917-9a663cf1c4c0"
+    organization = "tomporat247"
 
     workspaces {
-      name = "tom-qa"
+      name = "templates2"
     }
   }
 }
