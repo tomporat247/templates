5f39e75547a688566c23ab381ecea153dc97ae1d
tomporat247 <tomporat247@gmail.com>
2022-10-12 17:57:04 +0300

    xxx

--- misc/remote-backend-env0/backend.tf
+++ misc/remote-backend-env0/backend.tf
@@ -1,20 +1,10 @@
-#terraform {
-#  backend "remote" {
-#    hostname = "backend-pr9224.api.dev.env0.com"
-#    organization = "a24d5d61-8c2b-4a58-8917-9a663cf1c4c0"
-#
-#    workspaces {
-#      name = "tom-qa"
-#    }
-#  }
-#}
-
 terraform {
   backend "remote" {
-    organization = "tomporat247"
+    hostname = "backend-dev.api.dev.env0.com"
+    organization = "c2014ed1-c3b2-4ca9-b343-478e1f584fe0"
 
     workspaces {
-      name = "templates2"
+      prefix = "demo-u"
     }
   }
 }
