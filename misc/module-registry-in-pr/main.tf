62c2353f9a466ec026eede3d414da7805a8c9099
tomporat247 <tomporat247@gmail.com>
2022-12-05 15:20:51 +0200

    add

--- misc/module-registry-in-pr/main.tf
+++ misc/module-registry-in-pr/main.tf
@@ -1,4 +1,4 @@
 module "default-on-standard-github" {
   source = "api-pr10252.dev.env0.com/2ad15b8d-bec2-4ba3-a472-b8ea1b7f3ebb/default-on-standard-github/defaultonstandardgithub"
-  version = "v1.0.0"
+  version = "1.0.1"
 }
