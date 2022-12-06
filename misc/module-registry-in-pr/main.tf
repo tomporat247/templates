13e48466464c3ea5ceb125702f614cd3bff8e4fd
tomporat247 <tomporat247@gmail.com>
2022-12-06 16:30:43 +0200

    add

--- misc/module-registry-in-pr/main.tf
+++ misc/module-registry-in-pr/main.tf
@@ -1,4 +1,4 @@
-module "default-on-standard-github" {
-  source = "api-pr10252.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/saas-github/saasgithub"
-  version = "1.0.1"
+module "bbs" {
+  source = "api-pr10252.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/bbs/bbs"
+  version = "1.0.0"
 }
