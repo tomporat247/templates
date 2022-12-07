e61ec4afdffbd7fd81e1284d08f94ca81fabe185
tomporat247 <tomporat247@gmail.com>
2022-12-07 10:32:13 +0200

    add

--- misc/module-registry-in-pr/main.tf
+++ misc/module-registry-in-pr/main.tf
@@ -1,4 +1,4 @@
-module "bbs" {
-  source = "api-pr10252.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/bbs/bbs"
-  version = "1.0.0"
+module "bbs3" {
+  source = "api-pr10252.dev.env0.com/737bb6df-1d96-4cec-825b-1e957dc54b95/bbs3/bbs3"
+  version = "1.0.2"
 }
