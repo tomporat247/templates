08ba3d714d2cb5726af183190ff6e3c3528517e0
Tom Porat <tomporat247@gmail.com>
2025-06-18 16:30:19 +0300

    refactor: replace S3 bucket configuration with AWS identity outputs

--- aws/s3-bucket/s3/s3.tf
+++ aws/s3-bucket/s3/s3.tf
@@ -4,79 +4,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.0.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
   }
 }
 
-provider "aws" {
-  region  = "us-east-1"
-  endpoints {
-    s3 = "http://localhost:4566"
-  }
+# Get information about the current AWS user
+data "aws_caller_identity" "current" {}
+
+# Output the current AWS user information
+output "aws_user_id" {
+  value = data.aws_caller_identity.current.user_id
 }
 
-provider "random" {}
-
-resource "random_string" "random" {
-  length = "16"
-  special = "false"
-  min_lower = "16"
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "env0-s3-${random_string.random.result}"
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_ownership_controls" "website_bucket" {
-  bucket = aws_s3_bucket.website_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "website_bucket" {
-  bucket = aws_s3_bucket.website_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_acl" "website_bucket" {
-  depends_on = [aws_s3_bucket_ownership_controls.website_bucket, aws_s3_bucket_public_access_block.website_bucket]
-  bucket = aws_s3_bucket.website_bucket.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = "${aws_s3_bucket.website_bucket.id}"
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "Public-Access",
-  "Statement": [
-    {
-      "Sid": "Allow-Public-Access-To-Bucket",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.website_bucket.bucket}/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  key    = "index.html"
-  source = "../index.html"
-  content_type = "text/html"
+output "aws_arn" {
+  value = data.aws_caller_identity.current.arn
 }
