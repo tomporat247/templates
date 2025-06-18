60f1b2796495d85906fc47d8eb553d8009f4a24e
Tom Porat <tomporat247@gmail.com>
2025-06-18 15:57:37 +0300

    refactor: update S3 bucket configuration to use newer AWS provider syntax for ACLs

--- aws/s3-bucket/s3/s3.tf
+++ aws/s3-bucket/s3/s3.tf
@@ -12,8 +12,28 @@ resource "random_string" "random" {
 
 resource "aws_s3_bucket" "website_bucket" {
   bucket = "env0-s3-${random_string.random.result}"
-  acl    = "public-read"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "website_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.website_bucket, aws_s3_bucket_public_access_block.website_bucket]
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "website_bucket_policy" {
